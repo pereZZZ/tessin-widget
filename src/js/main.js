c19ff7d2cd4d24cf4b9edc9b15d38483a032b48f
Viktor <perezzz@meta.ua>
2018-07-06 18:38:52 +0300

    Fix for prod.

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,16 @@
-let block = document.getElementsByClassName('b-blockImg');
 
-fetch('https://api.tessin.se/1/projects/no-cors')
-  .then(res => res.json())
-  .then(res => {
-    for(let i=0; i < 3; i++){
-      block[0].children[i].children[0].children[0].setAttribute('style',
-        `background-image: url('${res.payload.projects[i].teaserImage}')`
-      );
-      block[0].children[i].children[1].innerHTML=`${res.payload.projects[i].publicTeaser}`;
-      block[0].children[i].children[0].setAttribute('href',`https://tessin.se/projekt/${res.payload.projects[i].hId}/`)
-    }
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  let block = document.getElementsByClassName('b-blockImg');
+
+  fetch('https://api.tessin.se/1/projects/no-cors')
+    .then(res => res.json())
+    .then(res => {
+      for(let i=0; i < 3; i++){
+        block[0].children[i].children[0].children[0].setAttribute('style',
+          `background-image: url('https://${res.payload.projects[i].teaserImage}')`
+        );
+        block[0].children[i].children[1].innerHTML=`${res.payload.projects[i].publicTeaser}`;
+        block[0].children[i].children[0].setAttribute('href',`https://tessin.se/projekt/${res.payload.projects[i].hId}/`)
+      }
+    });
+});
